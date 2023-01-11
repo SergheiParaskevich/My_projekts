6682544a3584eb4459ef2a1f8b4199af66529793
serghei paraschevici <paraskevichi777@gmail.com>
2023-01-11 18:16:01 +0100

    navLink_add

--- samurai-way/src/App.tsx
+++ samurai-way/src/App.tsx
@@ -1,23 +1,37 @@
 import React from 'react';
+
 import './App.css';
 import Profile from './components/Content';
 import Dialogs from './components/Dialogs';
 import Header from "./components/Header";
 import Nav from './components/Nav';
+import News from './components/Content/News';
+import Music from './components/Content/Music';
+import Settings from './components/Settings';
+import { BrowserRouter, Route , Routes} from 'react-router-dom';
 
 
 function App() {
   return (
+    
     <div className="app-wrapper">
       Hello, samurai! Let's go!
       <Header />
       <Nav />
       <div className="app-wrapper-content">
-        <Dialogs />
-        {/* <Profile/> */}
+      <Routes>
+        <Route path='/profile' element={<Profile/>}/>
+        <Route path='/messages' element={<Dialogs/>}/>
+        <Route path='/news' element={<News/>}/>
+        <Route path='/music' element={<Music/>}/>
+        <Route path='/settings' element={<Settings/>}/>
+      </Routes>
+        
       </div>
       
     </div>
+   
+    
   );
 }
 
--- samurai-way/src/components/Nav/index.tsx
+++ samurai-way/src/components/Nav/index.tsx
@@ -1,17 +1,52 @@
 import React from "react";
-import style from '../Nav/style.module.css'
+import style from "../Nav/style.module.css";
+import { NavLink } from "react-router-dom";
 
 export default function Nav() {
   return (
     <nav className={style.nav_menu}>
-      <div><a href="#">Profile</a></div>
-      <div><a href="#">Messages</a></div>
-      <div><a href="#">Messages</a></div>
-      <div><a href="#">News</a></div>
-      <div><a href="#">Music</a></div>
-      <div><a href="#">Settings</a></div>
+      <div>
+        <NavLink
+          className={({ isActive }) => {
+            return isActive ? style.active : "";
+          }}
+          to={"/profile"}
+        >
+          Profile
+        </NavLink>
+      </div>
+      <div>
+        <NavLink
+          className={({ isActive }) => (isActive ? style.active : "")}
+          to={"/messages"}
+        >
+          Messages
+        </NavLink>
+      </div>
+      <div>
+        <NavLink
+          className={({ isActive }) => (isActive ? style.active : "")}
+          to={"/news"}
+        >
+          News
+        </NavLink>
+      </div>
+      <div>
+        <NavLink
+          className={({ isActive }) => (isActive ? style.active : "")}
+          to={"/music"}
+        >
+          Music
+        </NavLink>
+      </div>
+      <div>
+        <NavLink
+          className={({ isActive }) => (isActive ? style.active : "")}
+          to={"/settings"}
+        >
+          Settings
+        </NavLink>
+      </div>
     </nav>
   );
 }
-
-
